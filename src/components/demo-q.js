efa2f621334a63d71e1f085dd2a9cd941a66249d
William Weng <Weng.William@hutchisonports.com>
2025-12-30 11:28:23 +0800

    feat: demo-q

---
+++ src/components/demo-q.js
@@ -0,0 +1,58 @@
+import { css, html } from 'lit';
+import { EzComponent } from '../utils/components.js';
+import { DemoIcon } from './demo-icon.js';
+
+export class DemoQ extends EzComponent {
+    static styles = [
+        ...EzComponent.styles,
+        css`
+            :host {
+                display: block;
+
+                --q-color-content: var(--q-color-content-x, inherit);
+                --q-color-author: var(--q-color-author-x, inherit);
+            }
+            #container {
+                display: flex;
+                align-items: center;
+                flex-direction: column;
+            }
+            demo-icon {
+                height: 2em;
+                width: 2em;
+                color: var(--q-color-content);
+            }
+            p {
+                font-weight: 800;
+                font-size: 1.4em;
+                font-family: sans-serif;
+                line-height: 1.4em;
+                text-align: center;
+                text-rendering: optimizeLegibility;
+                text-size-adjust: 100%;
+                unicode-bidi: isolate;
+                color: var(--q-color-content);
+            }
+            i {
+                display: flex;
+                justify-content: flex-end;
+                width: 100%;
+                font-size: 1em;
+                color: var(--q-color-author);
+            }
+        `,
+    ];
+    constructor() {
+        super('demo-q');
+    }
+    render() {
+        return html`
+            <div id="container">
+                <demo-icon category="tabler" name="queto-op" filled></demo-icon>
+                <p><slot name="content"></slot></p>
+                <i>--<slot name="author"></slot></i>
+            </div>
+        `;
+    }
+}
+customElements.define('demo-q', DemoQ);
--- src/index.js
+++ src/index.js
@@ -13,3 +13,4 @@ export { DemoFooter } from './components/demo-footer.js';
 export { DemoReference } from './components/demo-reference.js';
 export { DemoBusinessCard } from './components/demo-business-card.js';
 export { DemoNotice } from './components/demo-notice.js';
+export { DemoQ } from './components/demo-q.js';
